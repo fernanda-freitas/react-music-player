66d593df0b5095e1adde14ff5b31a9e9082ca08b
fernanda freitas <idea.fernanda@gmail.com>
2022-12-18 17:08:15 +0000

    implemented pause play logit and basic style

--- src/components/Player/Index.jsx
+++ src/components/Player/Index.jsx
@@ -2,7 +2,7 @@ import './Player.css';
 import { useEffect, useState } from "react";
 import useSound from "use-sound"; // for handling the sound
 import beatOfNature from "../../assets/the-beat-of-nature.mp3"; // importing the music
-// import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons for play and pause
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons for play and pause
 // import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"; // icons for next and previous track
 // import { IconContext } from "react-icons"; // for customazing the icons
 
@@ -10,8 +10,34 @@ const Player = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [play, { pause, duration, sound }] = useSound(beatOfNature);
 
+  const playingButton = () => {
+    if (isPlaying) {
+      pause();
+      setIsPlaying(false);
+    } else {
+      play();
+      setIsPlaying(true);
+    }
+  }
+
   return (
-    <button onClick={play}>Boop!</button>
+    <div>
+      <h2>Playing Now</h2>
+      <img className="musicCover" src="https://picsum.photos/200/200" alt="cover image"/>
+      <div>
+        <h3 className="title">Rubaiyyan</h3>
+        <p className="subTitle">Qala</p>
+      </div>
+      {isPlaying ? (
+          <button>
+            <AiFillPauseCircle className='play-pause-button' onClick={playingButton}/>
+          </button>
+        ) : (
+          <button>
+            <AiFillPlayCircle className='play-pause-button' onClick={playingButton}/>
+          </button>
+        )}
+    </div>
   )
 }
 
