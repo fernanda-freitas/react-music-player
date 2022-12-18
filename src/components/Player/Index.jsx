c0b085d60deec3a388a3d2e3dcdcacc15628f55a
fernanda freitas <idea.fernanda@gmail.com>
2022-12-18 16:32:44 +0000

    added song to assets

--- src/components/Player/Index.jsx
+++ src/components/Player/Index.jsx
@@ -1,14 +1,17 @@
 import './Player.css';
-// import { useEffect, useState } from "react";
-// import useSound from "use-sound"; // for handling the sound
-// import qala from "../assets/qala.mp3"; // importing the music
+import { useEffect, useState } from "react";
+import useSound from "use-sound"; // for handling the sound
+import beatOfNature from "../../assets/the-beat-of-nature.mp3"; // importing the music
 // import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons for play and pause
 // import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"; // icons for next and previous track
 // import { IconContext } from "react-icons"; // for customazing the icons
 
 const Player = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [play, { pause, duration, sound }] = useSound(beatOfNature);
+
   return (
-    <p>Lorem ipsum dolor</p>
+    <button onClick={play}>Boop!</button>
   )
 }
 
