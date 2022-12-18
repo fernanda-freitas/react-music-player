1c8ca3d3f511f7a3eb7e1a03d50ec991d5f4ee7f
fernanda freitas <idea.fernanda@gmail.com>
2022-12-18 18:16:53 +0000

    implemented time bar

--- src/components/Player/Index.jsx
+++ src/components/Player/Index.jsx
@@ -20,6 +20,44 @@ const Player = () => {
     }
   }
 
+  const [time, setTime] = useState({
+    min: "",
+    sec: ""
+  });
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: ""
+  });
+
+  const [seconds, setSeconds] = useState();
+
+  useEffect(() => {
+    if (duration) {
+      const sec = duration / 1000;
+      const min = Math.floor(sec / 60);
+      const secRemain = Math.floor(sec % 60);
+      setTime({
+        min: min,
+        sec: secRemain
+      });
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([]));
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min,
+          sec
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
   return (
     <div className='container'>
       <h2>Playing Now</h2>
@@ -27,6 +65,27 @@ const Player = () => {
       <div>
         <h3 className="title">Rubaiyyan</h3>
         <p className="subTitle">Qala</p>
+      </div>
+      <div className='time-bar'>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
       </div>
       <div className='controllers-container'>
         <div className='controllers'>
